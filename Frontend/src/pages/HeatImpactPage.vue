962e7a3882369c8f52da515edf496f1cdb10db5b
ZhangJianyu <jzha0488@student.monash.edu>
2025-04-13 14:44:46 +1000

    create 2 additional chart compoent and define the heat impact page layout

--- Frontend/src/pages/HeatImpactPage.vue
+++ Frontend/src/pages/HeatImpactPage.vue
@@ -1,11 +1,22 @@
 <template>
-  <line-chart></line-chart>
+  <div>
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-main>
+        <line-chart></line-chart>
+        <bar-chart></bar-chart>
+        <pie-chart></pie-chart>
+      </el-main>
+    </el-container>
+
+  </div>
+
 </template>
 
 <script setup>
 import LineChart from '@/components/LineChart.vue';
+import BarChart from '@/components/BarChart.vue';
+import PieChart from '@/components/PieChart.vue';
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
